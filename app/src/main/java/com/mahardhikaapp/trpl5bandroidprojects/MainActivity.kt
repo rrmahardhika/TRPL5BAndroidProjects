db700599394e09da9e9371578a015194331f0f94
Randy R Mahardhika <randy.mahardhika@jubelio.com>
2023-10-10 10:06:55 +0700

    implement navigations for course 3

--- app/src/main/java/com/mahardhikaapp/trpl5bandroidprojects/MainActivity.kt
+++ app/src/main/java/com/mahardhikaapp/trpl5bandroidprojects/MainActivity.kt
@@ -1,16 +1,31 @@
 package com.mahardhikaapp.trpl5bandroidprojects
 
+
+import android.content.Intent
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.mahardhikaapp.trpl5bandroidprojects.ui.theme.TRPL5BAndroidProjectsTheme
+import com.mahardhikaapp.trpl5bandroidprojects.screens.*
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +37,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ScreenMain()
                 }
             }
         }
@@ -30,17 +45,18 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name! this is new",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    TRPL5BAndroidProjectsTheme {
-        Greeting("Android Randy Change Example")
+fun ScreenMain() {
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = Routes.Home.route) {
+        composable(Routes.Home.route) {
+            Home(navController = navController)
+        }
+        composable(Routes.Profile.route) {
+            Profile()
+        }
+        composable(Routes.Setting.route + "/{no}") { navBackStack ->
+            val number = navBackStack.arguments?.getString("no")
+            Setting(number = number)
+        }
     }
 }
